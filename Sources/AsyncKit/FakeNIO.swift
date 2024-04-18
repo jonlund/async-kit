2e357a2aaac9aca2763040a1fccc56574b0e4645
Jon Lund <jon.lund@gmail.com>
2024-04-18 09:49:14 -0600

    start making fake NIO stuff

--- Sources/AsyncKit/FakeNIO.swift
+++ Sources/AsyncKit/FakeNIO.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+public enum TimeAmount {
+	case seconds(Int)
+}
+
+public struct Logger {
+	public init(label: String) {}
+}
+
+public class EventLoop {
+	
+//	public enum Key: String {
+//		case nothing
+//	}
+}
+
+public struct EventLoopGroup {
+}
+
+public struct NIOLock {
+	
+}
